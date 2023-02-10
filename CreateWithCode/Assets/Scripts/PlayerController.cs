c017d7cb4895d0991d4ecc22044bedf05295cbf6
IdkPoggersOrSumm <Somethingtodowithsharks@gmail.com>
2023-02-10 11:37:48 -0800

    Finished CarGame, Started Plane Challenge!

--- CreateWithCode/Assets/Scripts/PlayerController.cs
+++ CreateWithCode/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +18,9 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         //Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
     }
 }
