720a14457016cbc8b5d1116ac55cefe551dda150
IdkPoggersOrSumm <Somethingtodowithsharks@gmail.com>
2023-02-22 11:16:05 -0800

    Finished

--- CreateWithCode/Assets/Scripts/FollowPlayer.cs
+++ CreateWithCode/Assets/Scripts/FollowPlayer.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class FollowPlayer : MonoBehaviour
 {
     public GameObject player;
-    public Vector3 offset = new Vector3(0, 10, -14);
+    private Vector3 offset = new Vector3(0, 10, -14);
     // Start is called before the first frame update
     void Start()
     {
